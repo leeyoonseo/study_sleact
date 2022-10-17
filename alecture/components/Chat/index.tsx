03ee0a5c8f5f85524cb8b1ae0daac3d466036c43
leeyoonseo <okayoon.lee@gmail.com>
2022-10-17 21:38:24 +0900

    커스텀 스크롤바와 dayjs

--- alecture/components/Chat/index.tsx
+++ alecture/components/Chat/index.tsx
@@ -2,13 +2,15 @@ import React, { FC } from 'react'
 import gravatar from 'gravatar';
 import { IDM } from '@typings/db';
 import { ChatWrapper } from './styles'
+import dayjs from 'dayjs';
 
 interface Props {
   data: IDM;
 }
 
-const Chat: FC<Props> = ({ data }: Props) => {
+const Chat: FC<Props> = ({ data }) => {
   const user = data.Sender;
+  const createdAt = dayjs(data.createdAt).format('h:mm A');
 
   return (
     <ChatWrapper>
@@ -17,8 +19,8 @@ const Chat: FC<Props> = ({ data }: Props) => {
       </div>
       <div className="chat-text">
         <div className="chat-user">
-          <b>{user.nickname}</b>
-          <span>{String(data.createdAt)}</span>
+          <b>{user.nickname}</b>&nbsp;
+          <span>{createdAt}</span>
         </div>
         <p>{data.content}</p>
       </div>
--- alecture/components/ChatList/index.tsx
+++ alecture/components/ChatList/index.tsx
@@ -1,18 +1,26 @@
 import Chat from '@components/Chat';
 import { IDM } from '@typings/db';
-import React, { FC } from 'react'
+import React, { FC, useCallback, useRef } from 'react'
 import { ChatZone, Section } from './styles'
+import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
   chatData?: IDM[];
 }
 
 const ChatList: FC<Props> = ({ chatData }: Props) => {
+  const scrollbarRef = useRef(null);
+  const onScroll = useCallback(() => {
+
+  }, []);
+
   return (
     <ChatZone>
-      {chatData?.map(chat => (
-        <Chat key={chat.id} data={chat} />
-      ))}
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        {chatData?.map(chat => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   )
 }
